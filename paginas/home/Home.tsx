1dbf240b3563508c11ed738c6fa3eddd99cf29a7
Lucky2109 <lukao2109@gmail.com>
2024-02-19 16:31:33 -0300

    implementação entre back e front

--- paginas/home/Home.tsx
+++ paginas/home/Home.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import homeLogo from '../../assets/Anya.png'
 import './Home.css';
 
-
 function Home() {
     return (
         <>
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde! onii chan uwu</h2>
-              <p className='text-xl'>Quando sua opinião for pix vc manda blz???????</p>
+              <h2 className='text-5xl font-bold'>Seja bem vinde! onii-chan uwu</h2>
+              <p className='text-xl'>Quando seus pensamentos e opiniões forem pix vc manda blz????</p>
   
               <div className="flex justify-around gap-4">
               
@@ -24,7 +23,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        
       </>
     );
 }
