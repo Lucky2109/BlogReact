29468375cba4da1ad333e65b4e97d0761f91e0eb
Lucky2109 <lukao2109@gmail.com>
2024-02-27 01:45:50 -0300

    adicionando postagem pelo front

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
 import './App.css';
-
-import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
-import Cadastro from './paginas/cadastro/Cadastro';
-import Home from './paginas/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
-
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import Navbar from './components/navbar/Navbar';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
   return (
@@ -29,6 +29,10 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- paginas/home/Home.tsx
+++ paginas/home/Home.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import homeLogo from '../../assets/Anya.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+import { Link } from 'react-router-dom';
+
+
 
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-pink-600 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde! onii-chan uwu</h2>
-              <p className='text-xl'>Quando seus pensamentos e opiniões forem pix vc manda blz????</p>
+              <h2 className='text-5xl font-bold'>Seja bem vinde onii-chan!</h2>
+              <p className='text-xl'>quando seus pensamentos e opiniões forem pix vc manda blz??????</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <Link to='/postagens'>
+              <button className='border rounded px-4 hover:bg-white hover:text-indigo-800'>Ver postagens</button>
+              </Link>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -23,7 +30,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
